a3d7863219e9ef90d649be986dcda5349c91cdec
Vinícius Pereira <32654893+ViniciusP78@users.noreply.github.com>
2020-11-29 21:43:53 -0300

    Alterado a classe disciplina com SQLite

--- src/java/fatecpg/poo/p1/Disciplina.java
+++ src/java/fatecpg/poo/p1/Disciplina.java
@@ -5,7 +5,9 @@
  */
 package fatecpg.poo.p1;
 
+import java.sql.*;
 import java.util.ArrayList;
+import fatecpg.poo.p1.DBListener;
 
 /**
  *
@@ -13,15 +15,75 @@ import java.util.ArrayList;
  */
 public class Disciplina {
     
+    private long rowId;
     private String nome;
     private String ementa;
     private int ciclo;
     private double nota;
     
-    public Disciplina(String nome, String ementa, int ciclo) {
+    public static ArrayList<Disciplina> getList(){
+        ArrayList<Disciplina> list = new ArrayList<>();
+        Connection con = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        
+        try{
+            con = DBListener.getConnection();
+            stmt = con.createStatement();
+            rs = stmt.executeQuery("SELECT rowid, * FROM disciplina");
+            while(rs.next()){
+                list.add(new Disciplina(
+                        rs.getLong("rowid"),
+                        rs.getString("nome"),
+                        rs.getString("ementa"),
+                        rs.getInt("ciclo"),
+                        rs.getDouble("nota")
+                ));
+            }
+        }catch(Exception ex){
+            System.out.println(ex);
+        }
+        return list;
+    }
+    
+    public static void insert(String nome, String ementa, int ciclo, Double nota) {
+        Connection con = null;
+        PreparedStatement stmt = null;
+        
+        try {
+            con = DBListener.getConnection();
+            stmt = con.prepareStatement("INSERT INTO disciplina VALUES(?,?,?,?");
+            stmt.setString(1, nome);
+            stmt.setString(2, ementa);
+            stmt.setInt(3, ciclo);
+            stmt.setDouble(4, nota);
+            stmt.execute();
+        }catch(Exception ex){
+            System.out.println(ex);
+        }
+    }
+    
+    public static void delete(long rowid) {
+        Connection con = null;
+        PreparedStatement stmt = null;
+        
+        try{
+            con = DBListener.getConnection();
+            stmt = con.prepareStatement("DELETE FROM disciplina WHERE rowid=?");
+            stmt.setLong(1,rowid);
+            stmt.execute();
+        }catch(Exception ex){
+            System.out.println(ex);
+        }
+    }
+    
+    
+    public Disciplina(long rowid, String nome, String ementa, int ciclo, double nota) {
+        this.rowId = rowid;
         this.nome = nome;
         this.ementa = ementa;
         this.ciclo = ciclo;
+        this.nota = nota;
     }
     
     public String getNome(){
@@ -52,18 +114,21 @@ public class Disciplina {
         this.nota = nota;
     }
     
-    public static ArrayList getList() {
-        ArrayList<Disciplina> disciplinas = new ArrayList<Disciplina>();
-        
-        disciplinas.add(new Disciplina("Banco de Dados", "..", 4));
-        disciplinas.add(new Disciplina("Engenharia de Software III", "..", 4));
-        disciplinas.add(new Disciplina("Programação Orientada a Objetos", "..", 4));
-        disciplinas.add(new Disciplina("Linguagem de Programação IV", "..", 4));
-        disciplinas.add(new Disciplina("Segurança da Informação", "..", 4));
-        disciplinas.add(new Disciplina("Sistemas Operacionais II", "..", 4));
-        disciplinas.add(new Disciplina("Metodologia da Pesquisa Científico-Tecnológica", "..", 4));
-        
-        return disciplinas;
-    };
+    public long getRowId() {
+        return rowId;
+    }
+
+    public void setRowId(long rowId) {
+        this.rowId = rowId;
+    }
+    
+    public static String getCreateStatement(){
+        return "CREATE TABLE IF NOT EXISTS disciplina("
+                + "nome VARCHAR(50) UNIQUE NOT NULL,"
+                + "ementa VARCHAR(255) UNIQUE NOT NULL,"
+                + "ciclo INT NOT NULL,"
+                + "nota DOUBLE NOT NULL"
+                + ")";
+    }
     
 }
